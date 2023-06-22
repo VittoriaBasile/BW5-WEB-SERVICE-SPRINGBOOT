b2ad748c623e68a4abc9c9d04bad941bef02429e
VittoriaBasile <vichy.basile@gmail.com>
2023-06-22 13:09:12 +0200

    fix controller di cliente e fattura

--- src/main/java/epicode/EPICENERGYSERVICE/controllers/ClienteController.java
+++ src/main/java/epicode/EPICENERGYSERVICE/controllers/ClienteController.java
@@ -1,10 +1,14 @@
 package epicode.EPICENERGYSERVICE.controllers;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import epicode.EPICENERGYSERVICE.entities.Cliente;
+import epicode.EPICENERGYSERVICE.exceptions.NotFoundException;
 import epicode.EPICENERGYSERVICE.services.ClienteService;
 
 @RestController
@@ -42,6 +47,48 @@ public class ClienteController {
 	@GetMapping("/{utenteId}")
 	public Cliente readUtente(@PathVariable UUID clienteId) throws Exception {
 		return clienteService.findById(clienteId);
+	}
+
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Cliente>> getClientiByNome(@PathVariable String nome) {
+		try {
+			List<Cliente> clienti = clienteService.findByNome(nome);
+			return ResponseEntity.ok(clienti);
+		} catch (NotFoundException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@GetMapping("/fatturato/{fatturatoAnnuo}")
+	public ResponseEntity<List<Cliente>> getClientiByFatturatoAnnuo(@PathVariable double fatturatoAnnuo) {
+		try {
+			List<Cliente> clienti = clienteService.findByFatturatoAnnuo(fatturatoAnnuo);
+			return ResponseEntity.ok(clienti);
+		} catch (NotFoundException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@GetMapping("/data-inserimento/{data}")
+	public ResponseEntity<List<Cliente>> getClientiByDataInserimento(
+			@PathVariable("data") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate data) {
+		try {
+			List<Cliente> clienti = clienteService.findByDataInserimento(data);
+			return ResponseEntity.ok(clienti);
+		} catch (NotFoundException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@GetMapping("/data-ultimo-contatto/{data}")
+	public ResponseEntity<List<Cliente>> getClientiByDataUltimoContatto(
+			@PathVariable("data") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate data) {
+		try {
+			List<Cliente> clienti = clienteService.findByDataUltimoContatto(data);
+			return ResponseEntity.ok(clienti);
+		} catch (NotFoundException e) {
+			return ResponseEntity.notFound().build();
+		}
 	}
 
 	@PutMapping("/{utenteId}")
